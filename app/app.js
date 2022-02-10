3c56f9fc924cf12290188ecec8f5c155af647d04
simutis <54304022+SimasG@users.noreply.github.com>
2022-02-10 12:14:34 +0200

    progressed with the search bar functionality

--- app/app.js
+++ app/app.js
@@ -3,19 +3,30 @@ const searchContainer = document.querySelector('.search-container');
 const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+let value = 'simasg';
+
 async function getUserData () {
-    const response = await fetch('https://api.github.com/users/octocat');
+    const response = await fetch(`https://api.github.com/users/${value}`);
     return await response.json();
 }
 
 displayUserData = async () => {
-    getUserData();
+    // getUserData();
+
+    // async function getUserData () {
+    //     const response = await fetch(`https://api.github.com/users/${value}`);
+    //     return await response.json();
+    // }
+
     const data = await getUserData();
+    console.log(data)
     const date = new Date(data.created_at);
 
     document.querySelector('#profile-picture').src = data.avatar_url;
     document.querySelector('.name').textContent = data.name;
     document.querySelector('.handle').textContent = `@${data.login}`;
+    document.querySelector('.join-date').textContent = `Joined ${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+    document.querySelector('.bio').textContent = data.bio;
     document.querySelector('.repos').textContent = data.public_repos;
     document.querySelector('.followers').textContent = data.followers;
     document.querySelector('.following').textContent = data.following;
@@ -24,10 +35,17 @@ displayUserData = async () => {
     // fix null value display
     document.querySelector('.twitter').textContent = data.twitter_username;
     document.querySelector('.company').textContent = data.company;
-    document.querySelector('.join-date').textContent = `Joined ${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+    
+    const form = document.querySelector('form');
 
+    form.addEventListener('input', async (e) => {
+        const value = e.target.value;
+        console.log(value);
+        console.log(`https://api.github.com/users/${value}`);
 
-
+        const response = await fetch(`https://api.github.com/users/${value}`);
+        return await response.json();
+    })
 }
 
 displayUserData();
