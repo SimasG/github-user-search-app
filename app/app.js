1dc8640c13b1155d3f3cb0f857be95c63d439127
simutis <54304022+SimasG@users.noreply.github.com>
2022-02-11 13:03:58 +0200

    tinkered with the search functionality

--- app/app.js
+++ app/app.js
@@ -1,25 +1,21 @@
-const searchContainer = document.querySelector('.search-container');
+// const searchContainer = document.querySelector('.search-container');
 
 const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-let value = 'simasg';
 
-async function getUserData () {
-    const response = await fetch(`https://api.github.com/users/${value}`);
-    return await response.json();
+let username = 'octocat';
+
+async function getUser (username) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+
+    console.log(data);
+    displayUser(data);
 }
 
-displayUserData = async () => {
-    // getUserData();
-
-    // async function getUserData () {
-    //     const response = await fetch(`https://api.github.com/users/${value}`);
-    //     return await response.json();
-    // }
-
-    const data = await getUserData();
-    console.log(data)
+displayUser = async () => {
+    const data = await getUser();
     const date = new Date(data.created_at);
 
     document.querySelector('#profile-picture').src = data.avatar_url;
@@ -35,17 +31,42 @@ displayUserData = async () => {
     // fix null value display
     document.querySelector('.twitter').textContent = data.twitter_username;
     document.querySelector('.company').textContent = data.company;
-    
-    const form = document.querySelector('form');
 
-    form.addEventListener('input', async (e) => {
-        const value = e.target.value;
-        console.log(value);
-        console.log(`https://api.github.com/users/${value}`);
+    // const date = new Date(user.created_at);
 
-        const response = await fetch(`https://api.github.com/users/${value}`);
-        return await response.json();
-    })
+    // document.querySelector('#profile-picture').src = user.avatar_url;
+    // document.querySelector('.name').textContent = user.name;
+    // document.querySelector('.handle').textContent = `@${user.login}`;
+    // document.querySelector('.join-date').textContent = `Joined ${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+    // document.querySelector('.bio').textContent = user.bio;
+    // document.querySelector('.repos').textContent = user.public_repos;
+    // document.querySelector('.followers').textContent = user.followers;
+    // document.querySelector('.following').textContent = user.following;
+    // document.querySelector('.location').textContent = user.location;
+    // document.querySelector('.website').textContent = user.blog;
+    // // fix null value display
+    // document.querySelector('.twitter').textContent = user.twitter_username;
+    // document.querySelector('.company').textContent = user.company;
 }
 
-displayUserData();
+const form = document.querySelector('form');
+
+form.addEventListener('click', async (e) => {
+    // const value = e.target.value;
+    // console.log(value);
+    // console.log(`https://api.github.com/users/${value}`);
+
+    e.preventDefault();
+
+    const input = document.querySelector('#search');
+    const username = input.value.toLowerCase();
+
+    if (username) {
+        getUser(username)
+        input.value = '';
+    }
+  
+})
+
+// getUser(username);
+// displayUser();
