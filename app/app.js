f6e554f8529680a4db3fe04ed3cd31dcb18ce84a
simutis <54304022+SimasG@users.noreply.github.com>
2022-02-12 20:20:24 +0200

    progressed with the theme toggle functionality

--- app/app.js
+++ app/app.js
@@ -4,6 +4,21 @@ const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
 const input = document.querySelector('#search');
 const noResults = document.querySelector('.no-results');
 
+const avatar = document.querySelector('#profile-picture');
+const userName = document.querySelector('.name');
+const handle = document.querySelector('.handle');
+const joinDate = document.querySelector('.join-date');
+const bio = document.querySelector('.bio');
+const publicRepos = document.querySelector('.repos');
+const followers = document.querySelector('.followers');
+const following = document.querySelector('.following');
+const userLocation = document.querySelector('.location');
+const website = document.querySelector('.website');
+const twitterUsername = document.querySelector('.twitter')
+const company = document.querySelector('.company')
+
+
+
 
 let username = 'octocat';
 
@@ -31,21 +46,21 @@ displayNoResults = (user) => {
 displayUser = async (user) => {
     const date = new Date(user.created_at);
 
-    const avatar = document.querySelector('#profile-picture');
+    // const avatar = document.querySelector('#profile-picture');
     if (!user.avatar_url) {
         avatar.src = "https://avatars.githubusercontent.com/u/583231?v=4";
     } else {
         avatar.src = user.avatar_url;
     }
 
-    const userName = document.querySelector('.name');
+    // const userName = document.querySelector('.name');
     if (!user.name) {
         userName.textContent = 'Not Available';
     } else {
         userName.textContent = user.name;
     }
 
-    const handle = document.querySelector('.handle');
+    // const handle = document.querySelector('.handle');
     if (!user.login) {
         handle.textContent = 'Not Available';
         // noResults.classList.add('active-inline');
@@ -53,64 +68,63 @@ displayUser = async (user) => {
         handle.textContent = `@${user.login}`;
     }
 
-    const joinDate = document.querySelector('.join-date');
+    // const joinDate = document.querySelector('.join-date');
     if (!user.created_at) {
         joinDate.textContent = 'Not Available';
     } else {
         joinDate.textContent = `Joined ${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`
     }
 
-    const bio = document.querySelector('.bio');
+    // const bio = document.querySelector('.bio');
     if (!user.bio) {
-        bio.textContent = 'Not Available'
+        bio.textContent = 'This profile has no bio'
     } else {
         bio.textContent = user.bio;
     }
 
-    const publicRepos = document.querySelector('.repos');
+    // const publicRepos = document.querySelector('.repos');
     if (!user.public_repos) {
         publicRepos.textContent = 'N/A'
     } else {
         publicRepos.textContent = user.public_repos;
     }
 
-    const followers = document.querySelector('.followers');
+    // const followers = document.querySelector('.followers');
     if (!user.followers) {
         followers.textContent = 'N/A'
     } else {
         followers.textContent = user.followers;
     }
 
-    const following = document.querySelector('.following');
+    // const following = document.querySelector('.following');
     if (!user.following) {
         following.textContent = 'N/A'
     } else {
         following.textContent = user.following;
     }
 
-    const location = document.querySelector('.location');
+    // const userLocation = document.querySelector('.location');
     if (!user.location) {
-        location.textContent = 'Not Available';
+        userLocation.textContent = 'Not Available';
     } else {
-        location.textContent = user.location;
+        userLocation.textContent = user.location;
     }
 
-    const website = document.querySelector('.website');
+    // const website = document.querySelector('.website');
     if (!user.blog) {
         website.textContent = 'Not Available'
     } else {
         website.textContent = user.blog;
     }
 
-    const twitterUsername = document.querySelector('.twitter')
+    // const twitterUsername = document.querySelector('.twitter')
     if (!user.twitter_username) {
         twitterUsername.textContent = 'Not Available'
     } else {
         twitterUsername.textContent = user.twitter_username;
     }
 
-    document.querySelector('.company').textContent = user.company;
-    const company = document.querySelector('.company')
+    // const company = document.querySelector('.company')
     if (!user.company) {
         company.textContent = 'Not Available'
     } else {
@@ -143,3 +157,52 @@ form.addEventListener('click', () => {
 
 
 getUser(username);
+
+
+
+
+
+// THEME FUNCTIONALITY
+const themeBtn = document.querySelector('.modes');
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+const lightBtn = document.querySelector('.light');
+const darkBtn = document.querySelector('.dark');
+const searchContainer = document.querySelector('.search-container');
+const mainContainer = document.querySelector('.main-container');
+const statsContainer = document.querySelector('.stats-container');
+// const statsHeading = document.querySelector('.stats-heading');
+
+// const statsHeading1 = document.querySelector('.stats-heading-1');
+// const statsHeading2 = document.querySelector('.stats-heading-2');
+// const statsHeading3 = document.querySelector('.stats-heading-3');
+
+
+
+
+themeBtn.addEventListener('click', () => {
+    body.style.backgroundColor = '#141D2F';
+    logo.style.color = '#FFFFFF';
+    lightBtn.classList.remove('hide');
+    darkBtn.classList.add('hide');
+    input.classList.add('display-placeholder');
+    searchContainer.style.backgroundColor = '#1E2A47';
+    mainContainer.style.backgroundColor = '#1E2A47';
+    statsContainer.style.backgroundColor = '#141D2F';
+    userName.style.color = '#FFFFFF';
+    joinDate.style.color = '#FFFFFF';
+    bio.style.color = '#FFFFFF';
+    // don't know why it doesn't turn all the stats headings white
+    // statsHeading1.style.color = '#FFFFFF';
+    // statsHeading2.style.color = '#FFFFFF';
+    // statsHeading3.style.color = '#FFFFFF';
+
+    publicRepos.style.color = '#FFFFFF';
+    followers.style.color = '#FFFFFF';
+    following.style.color = '#FFFFFF';
+    
+    userLocation.style.color = '#FFFFFF';
+    website.style.color = '#FFFFFF';
+    twitterUsername.style.color = '#FFFFFF';
+    company.style.color = '#FFFFFF';
+})
